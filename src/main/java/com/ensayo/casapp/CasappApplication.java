7606fc6312d2bf4e0074f56875ec73bb87e1a612
Agudelo3075 <sebasagudelo1012@gmail.com>
2025-06-11 12:36:43 -0500

    Agregando primeros archivos

---
+++ src/main/java/com/ensayo/casapp/CasappApplication.java
@@ -0,0 +1,13 @@
+package com.ensayo.casapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensayo/casapp/CasappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensayo.casapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
